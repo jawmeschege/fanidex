87330f5c37e2b86c63eb88b45a2dd418738e0084
james chege <jawiwy@gmail.com>
2021-11-28 11:01:17 +0400

    Updates

---
+++ precache-manifest.a65ca81dc05357bb93c73af8b5f3a04a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1bc5187652257aa0e1b2aa1d4045a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff7377b3d9aceecff37",
+    "url": "/static/css/2.3a0ac4c9.chunk.css"
+  },
+  {
+    "revision": "983aa189819b50036942",
+    "url": "/static/css/main.79052ad6.chunk.css"
+  },
+  {
+    "revision": "8ff7377b3d9aceecff37",
+    "url": "/static/js/2.e01f09a1.chunk.js"
+  },
+  {
+    "revision": "958608aaef3e81c9b20a22457416f406",
+    "url": "/static/js/2.e01f09a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983aa189819b50036942",
+    "url": "/static/js/main.5d8a050d.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/static/media/logo.49174c73.svg"
+  }
+]);
